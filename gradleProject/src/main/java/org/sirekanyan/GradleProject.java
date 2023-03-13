01143a5556de7b81834091c0e3070f0e5c80bf73
sirekanian <sirekanian@pm.me>
2023-03-13 20:23:34 +0400

    init

---
+++ gradleProject/src/main/java/org/sirekanyan/GradleProject.java
@@ -0,0 +1,7 @@
+package org.sirekanyan;
+
+public class GradleProject {
+    public static void main() {
+        System.out.println("Hello world from Gradle Project!");
+    }
+}
---
+++ mavenProject/src/main/java/org/sirekanyan/Main.java
@@ -0,0 +1,7 @@
+package org.sirekanyan;
+
+public class Main {
+    public static void main(String[] args) {
+        GradleProject.main();
+    }
+}
